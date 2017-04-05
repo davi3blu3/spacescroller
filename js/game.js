951d0622ae659d0c9f3797d7ee8dc3e72f0cb09e
Dave Harned <dharned@gmail.com>
2017-04-04 20:21:09 -0500

    add player movement

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
 
 var starfield,
-    player;
+    player,
+    cursors;
 
 var mainState = {
     preload: function() {
@@ -20,6 +21,9 @@ var mainState = {
     },
 
     create: function() {
+        // enable Arcade Physics
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
         // create starry background
         starfield = game.add.tileSprite(0,0,800,600, 'starfield');
 
@@ -29,12 +33,40 @@ var mainState = {
         game.add.sprite(600, 500, 'venus');
         game.add.sprite(500, 400, 'moon');
 
+        // create player and add properties
         player = game.add.sprite(50, 280, 'ship');
+        game.physics.arcade.enable(player);
+        player.body.collideWorldBounds = true;
+
+        // enable controls
+        cursors = game.input.keyboard.createCursorKeys();
+
     },
 
     update: function() {
         // scroll background
         starfield.tilePosition.x += -2; 
+
+        //  Spaceshipt movement - reset velocity
+        player.body.velocity.x = 0;
+
+        if (cursors.up.isDown){
+            //  Move up
+            player.body.velocity.y = -150;
+        } else if (cursors.down.isDown){
+            //  Move down
+            player.body.velocity.y = 150;
+        } else if (cursors.left.isDown){
+            //  Move down
+            player.body.velocity.x = -150;
+        } else if (cursors.right.isDown){
+            //  Move down
+            player.body.velocity.x = 150;
+        } else {
+            //  Stand still
+            player.body.velocity.x = 0;
+            player.body.velocity.y = 0;
+        }
     }
 }
 
