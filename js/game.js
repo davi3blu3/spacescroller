8024a090a996f8ec11530f21f2e7d7b0a0dbc2c4
Dave Harned <dharned@gmail.com>
2017-04-04 19:37:12 -0500

    add assets and size planets

--- js/game.js
+++ js/game.js
@@ -7,15 +7,27 @@ var mainState = {
         // load starry background
         game.load.image('starfield', './assets/starfield.jpg');
 
+        // load obstacles
+        game.load.image('jupiter', 'assets/planets/jupiter.png');
+        game.load.image('saturn', 'assets/planets/saturn.png');
+        game.load.image('venus', 'assets/planets/venus.png');
+        game.load.image('moon', 'assets/planets/moon.png');
+
     },
 
     create: function() {
         // create starry background
         starfield = game.add.tileSprite(0,0,800,600, 'starfield');
+
+        // create planets
+        game.add.sprite(600, 0, 'jupiter');
+        game.add.sprite(600, 200, 'saturn');
+        game.add.sprite(600, 500, 'venus');
+        game.add.sprite(500, 400, 'moon');
     },
 
     update: function() {
-        // scrolling background
+        // scroll background
         starfield.tilePosition.x += -2; 
     }
 }
